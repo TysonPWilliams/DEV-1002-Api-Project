e93f9201b2caea258e5c294a8d485ab8f99d41b6
TysonPWilliams <tyson.williams95@gmail.com>
2025-03-04 17:49:55 +1100

    Finished CRUD and Blueprint for Contract

--- models/contract.py
+++ models/contract.py
@@ -16,16 +16,25 @@ class Contract(db.Model):
 
     freelancer = db.relationship('User', foreign_keys=[freelancer_id], back_populates='freelancer_contracts')
     client = db.relationship('User', foreign_keys=[client_id], back_populates='client_contracts')
+    job = db.relationship('Job', back_populates='contract')
 
     
 
 class ContractSchema(ma.Schema):
+    
     created_at = fields.DateTime(format="%Y-%m-%d")
-    freelancer_id = fields.Nested('UserSchema', exclude=['role', 'address'])
     client_id = fields.Nested('UserSchema', exclude=['role', 'address'])
 
     class Meta:
-        fields = ('id', 'job_id', 'freelancer_id', 'freelancer' 'client_id', 'start_date', 'end_date', 'created_at')
+        model = Contract
+        load_instance = True
+        fields = ('id', 'job', 'freelancer', 'client', 'start_date', 'end_date', 'created_at')
+
+    
+
+    freelancer = fields.Nested('UserSchema', exclude=['role', 'address'])
+    client = fields.Nested('UserSchema', exclude=['role', 'address'])
+    job = fields.Nested('JobSchema', exclude=['client_id', 'client', 'created_at'])
 
 one_contract = ContractSchema()
 many_contracts = ContractSchema(many=True)
